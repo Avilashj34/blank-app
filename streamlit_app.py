0c523bec47cb1eb93d62dbd5dfdf3addee4a2553
Avilashj34 <avilashjha97@gmail.com>
2025-02-06 21:46:25 +0530

    Updated file

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+st.title("📊 SMS Cost Analysis Dashboard")
+uploaded_file = st.file_uploader("Upload Excel File", type=["xlsx"])
+
+
+st.success("Data vendor")
